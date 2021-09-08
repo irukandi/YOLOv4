eb18b96dc46032f7708395268981d0c5df3e38a4
manel <malikmanel@gmail.com>
2021-09-08 20:50:00 +0100

    Added support for data without annotations

--- data_handling.py
+++ data_handling.py
@@ -8,14 +8,17 @@ import random
 
 
 class Data(Dataset):
-    def __init__(self, folder_path, data_file_path, images_path, classes_file_path, header=None, transform=None):
+    def __init__(self, folder_path, images_path, classes_file_path, annotations_file_path=None, header=None, transform=None):
         super().__init__()
         if header is None:
             header = ['img_path', 'tl_x', 'tl_y', 'br_x', 'br_y', 'class']
         if transform is None:
             transform = transforms.ToTensor()
+        if annotations_file_path is None:
+            self.annotations = None
+        else:
+            self.annotations = pd.read_csv(folder_path + annotations_file_path, names=header)
         self.images = get_image_files(folder_path + images_path)
-        self.annotations = pd.read_csv(folder_path + data_file_path, names=header)
         self.transform = transform
         self.classes = read_labels(folder_path + classes_file_path)
 
@@ -25,21 +28,23 @@ class Data(Dataset):
     def __getitem__(self, index):
         image_path = str(self.images[index])
         image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-        target = {}
-        labels, boxes = get_target_data(image_path, self.annotations)
-        labels = labels_to_id(labels, self.classes)
-        iscrowd = torch.zeros((boxes.shape[0],))
-        image_id = torch.tensor([index])
-        labels = torch.as_tensor(labels, dtype=torch.float32)
-        boxes = torch.as_tensor(boxes, dtype=torch.int32)
-        target['boxes'] = boxes
-        target['labels'] = labels
-        target['iscrowd'] = iscrowd
-        target['image_id'] = image_id
+        # image = torch.as_tensor(image, dtype=torch.int32)#self.transform(image)
+        if self.annotations is not None:
+            target = {}
+            labels, boxes = get_target_data(image_path, self.annotations)
+            labels = labels_to_id(labels, self.classes)
+            iscrowd = torch.zeros((boxes.shape[0],))
+            image_id = torch.tensor([index])
+            labels = torch.as_tensor(labels, dtype=torch.float32)
+            boxes = torch.as_tensor(boxes, dtype=torch.int32)
+            target['boxes'] = boxes
+            target['labels'] = labels
+            target['iscrowd'] = iscrowd
+            target['image_id'] = image_id
 
-        #image = torch.as_tensor(image, dtype=torch.int32)#self.transform(image)
-
-        return image, target
+            return image, target
+        else:
+            return image
 
     def example_image(self, index=None):
         if index is None:
@@ -48,13 +53,15 @@ class Data(Dataset):
             image_path = str(self.images[index])
 
         image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-        labels, boxes = get_target_data(image_path, self.annotations)
 
-        for idx in range(len(boxes)):
-            box = boxes[idx]
-            label = labels[idx]
-            cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 2)
-            cv2.putText(image, label, (box[0], box[1] - 5), 0, 0.5, (255, 0, 0))
+        if self.annotations is not None:
+            labels, boxes = get_target_data(image_path, self.annotations)
+
+            for idx in range(len(boxes)):
+                box = boxes[idx]
+                label = labels[idx]
+                cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 2)
+                cv2.putText(image, label, (box[0], box[1] - 5), 0, 0.5, (255, 0, 0))
 
         cv2.imshow('Image', image)
         cv2.waitKey(0)
@@ -81,6 +88,6 @@ def labels_to_id(labels, labels_id):
 
 root = "Example_Dataset/"
 
-data = Data(root, data_file_path="train/_annotations.csv", images_path="train", classes_file_path="class_names.csv")
+data = Data(root, images_path="train", classes_file_path="class_names.csv", annotations_file_path="train/_annotations.csv")
 
 data.example_image()
