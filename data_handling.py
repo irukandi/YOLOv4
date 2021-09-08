d5e21bd8597885b8b1992a6afb5eceaebcda13f1
manel <malikmanel@gmail.com>
2021-09-08 16:11:16 +0100

    Finished data_handling.py (may need some tweaks)

--- data_handling.py
+++ data_handling.py
@@ -1,64 +1,86 @@
-import os
 import cv2
-import matplotlib.pyplot as plt
 import torch
-#from pathlib import Path
-#from fastai import *
 import torchvision.transforms as transforms
 from fastai.vision.all import get_image_files
-from fastai.imports import Path
 import pandas as pd
 from torch.utils.data import Dataset
+import random
 
 
 class Data(Dataset):
-    def __init__(self, csv_file, images_path, header=None, transform=None):
+    def __init__(self, folder_path, data_file_path, images_path, classes_file_path, header=None, transform=None):
         super().__init__()
         if header is None:
             header = ['img_path', 'tl_x', 'tl_y', 'br_x', 'br_y', 'class']
         if transform is None:
             transform = transforms.ToTensor()
-        self.images = get_image_files(images_path)
-        self.annotations = pd.read_csv(csv_file, names=header)
+        self.images = get_image_files(folder_path + images_path)
+        self.annotations = pd.read_csv(folder_path + data_file_path, names=header)
         self.transform = transform
+        self.classes = read_labels(folder_path + classes_file_path)
 
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, index):
-        img_path = self.images[index].name
-        image = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+        image_path = str(self.images[index])
+        image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
         target = {}
-        labels, boxes = get_target_data(img_path, self.annotations)
+        labels, boxes = get_target_data(image_path, self.annotations)
+        labels = labels_to_id(labels, self.classes)
         iscrowd = torch.zeros((boxes.shape[0],))
         image_id = torch.tensor([index])
-        print(labels) # need to convert labels to int
         labels = torch.as_tensor(labels, dtype=torch.float32)
-        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        boxes = torch.as_tensor(boxes, dtype=torch.int32)
         target['boxes'] = boxes
         target['labels'] = labels
         target['iscrowd'] = iscrowd
         target['image_id'] = image_id
 
-        image = self.transform(image)
+        #image = torch.as_tensor(image, dtype=torch.int32)#self.transform(image)
 
         return image, target
 
+    def example_image(self, index=None):
+        if index is None:
+            image_path = str(random.choice(self.images))
+        else:
+            image_path = str(self.images[index])
 
-def get_target_data(img_path, data_frame):
+        image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+        labels, boxes = get_target_data(image_path, self.annotations)
+
+        for idx in range(len(boxes)):
+            box = boxes[idx]
+            label = labels[idx]
+            cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 2)
+            cv2.putText(image, label, (box[0], box[1] - 5), 0, 0.5, (255, 0, 0))
+
+        cv2.imshow('Image', image)
+        cv2.waitKey(0)
+
+
+def get_target_data(image_path, data_frame):
+    image_name = image_path.rsplit("/", 1)[-1]
     bbox_columns = ['tl_x', 'tl_y', 'br_x', 'br_y']
-    rows = data_frame[data_frame.iloc[:, 0] == img_path]
+    rows = data_frame[data_frame.iloc[:, 0] == image_name]
+
     return rows['class'].values, rows[bbox_columns].values
 
 
-root = Path("Example_Dataset")
+def read_labels(classes_file_path):
+    class_labels = pd.read_csv(classes_file_path, header=None)
+    class_labels = dict(class_labels.values)
+    return class_labels
 
-train_images = get_image_files(root/"test")
 
-img = cv2.imread(str(train_images[-1]), cv2.IMREAD_UNCHANGED)
-img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+def labels_to_id(labels, labels_id):
+    labels = [labels_id[label] for label in labels]
+    return labels
 
-data = Data(csv_file=root/"test/_annotations.csv", images_path=root/"test")
 
-data.__getitem__(3)
+root = "Example_Dataset/"
 
+data = Data(root, data_file_path="train/_annotations.csv", images_path="train", classes_file_path="class_names.csv")
+
+data.example_image()
